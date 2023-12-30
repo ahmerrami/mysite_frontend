c223be6077ac3afbd9a46060deccdfe7618777c6
ahmederrami <ahmederrami@gmail.com>
2023-12-30 18:34:53 +0100

    toggle color added to omra button

--- src/pages/Header/Header.jsx
+++ src/pages/Header/Header.jsx
@@ -1,6 +1,21 @@
+import React, { useState, useEffect } from 'react';
 import styles from './Header.module.css';
 
 function Header({setActiveContent}){
+    const [buttonColor, setButtonColor] = useState('black');
+    useEffect(() => {
+        // Function to be executed inside setInterval
+        const changeColor = () => {
+          // Toggle the color between red and blue
+          setButtonColor((prevColor) => (prevColor === 'red' ? 'blue' : 'red'));
+        };
+    
+        // Set up a setInterval to call changeColor every 1000 milliseconds (1 second)
+        const intervalId = setInterval(changeColor, 1000);
+    
+        // Cleanup the interval when the component is unmounted
+        return () => clearInterval(intervalId);
+      }, []);
     const imageUrl = './Logo ST PNG.png';
     // Function to handle menu item clicks
     const handleMenuClick = (content) => {
@@ -13,7 +28,7 @@ function Header({setActiveContent}){
             <button onClick={() => handleMenuClick('home')}>Home</button>
             <button onClick={() => handleMenuClick('about')}>About</button>
             <button onClick={() => handleMenuClick('reference')}>Référence</button>
-            <button onClick={() => handleMenuClick('omra')}>Omra</button>
+            <button onClick={() => handleMenuClick('omra')} style={{ backgroundColor: buttonColor, color:'white' }}>Omra</button>
             <button onClick={() => handleMenuClick('contact')}>Contact</button>
         </header>
     );
