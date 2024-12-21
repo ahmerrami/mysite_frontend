dc1072cb717a3c8dbe5aed68e6b92364cdc4179c
ahmederrami <ahmederrami@gmail.com>
2024-12-22 00:57:23 +0100

    AO en bdd

--- src/pages/AO/AO.jsx
+++ src/pages/AO/AO.jsx
@@ -1,20 +1,43 @@
-// import React from 'react';
+import React, { useState, useEffect } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css'; // Import Bootstrap CSS
-import { Table } from 'react-bootstrap'; // Import Table component from React Bootstrap
+import { Table, Spinner, Alert } from 'react-bootstrap'; // Import Table, Spinner, and Alert components
+import axios from 'axios';
 
 import PDFDownloader from '../PDFViewer/PDFDownloader';
 
 const AO = () => {
-  const AOs = [
-    { 
-      id: 1,
-      numero: "08-AO-2024-ST",
-      pdfUrl: '/08-AO-2024-ST.pdf', 
-      objet: "Mise à disposition du personnel pour assurer la prestation d'exploitation au niveau des parkings et la prestation de services communs au niveau du siège", 
-      date: '22 Novembre 2024',
-    }
-  ];
-  
+  const [AOs, setAOs] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    // Remplacez l'URL par l'endpoint réel de votre API Django REST
+    const fetchAOs = async () => {
+      try {
+        const response = await axios.get('https://errami.pythonanywhere.com/api/aos/aos/');
+        setAOs(response.data);
+        setLoading(false);
+      } catch (err) {
+        setError('Erreur lors du chargement des données.');
+        setLoading(false);
+      }
+    };
+
+    fetchAOs();
+  }, []);
+
+  if (loading) {
+    return <Spinner animation="border" />;
+  }
+
+  if (error) {
+    return <div className="text-danger">{error}</div>;
+  }
+
+  if (AOs.length === 0) {
+    return <Alert variant="info">Aucun appel d'offres disponible pour le moment.</Alert>;
+  }
+
   return (
     <Table striped bordered hover>
       <thead>
@@ -31,7 +54,7 @@ const AO = () => {
             <td>{AO.numero}</td>
             <td>{AO.objet}</td>
             <td>{AO.date}</td>
-            <td><PDFDownloader pdfUrl={AO.pdfUrl} /></td>
+            <td><PDFDownloader pdfUrl={AO.aopdf} /></td>
           </tr>
         ))}
       </tbody>
