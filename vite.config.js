01c98c539db89d0d27227c84910cef9de8e11a82
ahmederrami <ahmederrami@gmail.com>
2024-12-21 23:12:28 +0100

    version du 21 DEC 2024

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,18 @@
+/* eslint-env node */
+
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: process.env.VITE_BASE_URL || '/',  // Remplacez par le nom de votre dépôt
   plugins: [react()],
+  resolve: {
+    extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json'],  // Inclut l'extension `.jsx`
+  },
   build: {
     rollupOptions: {
       output: {
-        entryFileNames: `[name].js`, // Génère les fichiers d’entrée en `.js`
+        entryFileNames: `[name].js`,       // Fichiers JS générés
         chunkFileNames: `[name].js`,
         assetFileNames: `[name].[ext]`
       },
