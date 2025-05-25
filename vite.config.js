eff992f9f3e2740f92b362373d370e0203ef6795
SupratoursTravel <supratourstravel2009@gmail.com>
2025-05-25 13:05:45 +0100

    test deploiement via github actions

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: process.env.VITE_BASE_URL || '/',  // Remplacez par le nom de votre dépôt
+  base: '/', //process.env.VITE_BASE_URL || '/',  // Remplacez par le nom de votre dépôt
   plugins: [react()],
   resolve: {
     extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json'],  // Inclut l'extension `.jsx`
